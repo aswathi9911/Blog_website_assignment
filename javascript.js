98fccc4dabfc5744efcca3c1346bb4d603bf04f5
aswathi9911 <148189716+aswathi9911@users.noreply.github.com>
2025-09-18 00:32:34 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// Toggle Menu for Mobile
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
